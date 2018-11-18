61b04b9494ae410b376964f23e99b72b9a06e1be
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-11-18 21:40:53 +0530

    Saving files to github to cleanup local.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,NgbModule.forRoot(), HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
